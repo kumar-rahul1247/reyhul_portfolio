030452ee0e6aecafae575fe5b20017818a4ec571
kumar-rahul1247 <kumar.rahul1247@gmail.com>
2020-10-24 04:52:31 +0530

    Updates

---
+++ precache-manifest.213ddf352307cc5ada2e347f26510070.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710fe5e239476f7d2fd2eab4f5f0cda0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9cac2287dc8f0a5b916",
+    "url": "/static/css/main.5db945d7.chunk.css"
+  },
+  {
+    "revision": "6897035d1e265e5e583d",
+    "url": "/static/js/2.bdd7446a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bdd7446a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9cac2287dc8f0a5b916",
+    "url": "/static/js/main.a9754e58.chunk.js"
+  },
+  {
+    "revision": "e43c1cebc829ab3fe405",
+    "url": "/static/js/runtime-main.20a1a66e.js"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "5e2bc63bd2637fcc737cf56f1fa1dbeb",
+    "url": "/static/media/javaLogo.5e2bc63b.png"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "69b02d6c1934fd58c71785ea4980b73b",
+    "url": "/static/media/mongodbLogo.69b02d6c.png"
+  },
+  {
+    "revision": "269ff81336bdcd2a263973ddb50ae98f",
+    "url": "/static/media/mysqlLogo.269ff813.png"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "25e63889dbfb6aef7aea8352ef60dbae",
+    "url": "/static/media/reactLogo.25e63889.png"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b9d7c749caa9467d4e508dc166bf3d92",
+    "url": "/static/media/reyh_pic.b9d7c749.jpg"
+  },
+  {
+    "revision": "e704113fa801bd3409a55b4bbebfe787",
+    "url": "/static/media/roaddark.e704113f.jpg"
+  }
+]);
