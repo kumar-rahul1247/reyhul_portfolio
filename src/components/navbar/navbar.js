54b188740153ccb807e12fd81b9c2ea743db72b0
kumar-rahul1247 <kumar.rahul1247@gmail.com>
2020-10-30 01:37:27 +0530

    hide Experience Navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -41,7 +41,7 @@ const Navbar = () => {
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#skills">Skills</a></li>
-                    <li><a href="#experience">Experience</a></li>
+                    {/* <li><a href="#experience">Experience</a></li> */}
                     <li><a href="#projects">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
